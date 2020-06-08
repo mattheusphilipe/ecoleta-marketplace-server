c140a113dfce7ce3558f4247c335780c98211742
Matheus Felipe Araújo <matheus151929@live.com>
2020-06-08 18:17:49 -0300

    Update ItemsControllers.js

--- src/controllers/ItemsControllers.js
+++ src/controllers/ItemsControllers.js
@@ -3,12 +3,11 @@ const knex = require('../database/connection');
 class ItemsController {
 
     async index(request, response) {
-        const whichPort = process.env.PORT || 3232;
         try {
   
             const items = await knex('collected_items').select('*');
             const serializedItems = items.map(({title, image, id}) => 
-                ({id, title, image_url: `${process.env.APP_URL}:${whichPort}/uploads/${image}`}));
+                ({id, title, image_url: `${process.env.APP_URL}uploads/${image}`}));
         
             response.json(serializedItems);
 
